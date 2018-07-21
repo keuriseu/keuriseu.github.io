fd6cc850a5f591776f7e565da838e4f8cc9887d8
keuriseu <cris.olavario@gmail.com>
2018-07-21 14:06:03 +0900

    final update in korea

---
+++ filelist.py
@@ -0,0 +1,16 @@
+import os
+
+DIR = 'assets/img/'
+MON = '1807/'
+
+PRE = '![](/'
+SUF = ' "")'
+
+def main():
+    flist = os.listdir(DIR+MON)
+    for f in flist:
+        s = PRE+DIR+MON+f+SUF
+        print(s)
+
+if __name__ == "__main__":
+    main()
